372e4e661f7e20ecea846b05f18e689bf7190d4f
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-07-25 10:13:50 +0300

    :star: feat: handle new answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,24 @@
-export default function Options({ options }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
-    <>
-      {options.map((option) => (
-        <button className="btn btn-option" key={option}>
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
-    </>
+    </div>
   );
 }
